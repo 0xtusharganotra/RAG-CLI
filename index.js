f9d1b6d0ed600e87887be0063f1add42f96eb003
0xtusharganotra <ganotra.vox@gmail.com>
2026-01-09 08:03:31 +0530

    implement partial tool calling

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 async function main() {
   const args = process.argv[2];
   const chatCompletion = await getGroqChatCompletion(args);
+  const toolcall = chatCompletion.choices[0].message.tool_calls;
+  if (!toolcall) {
+    console.log(chatCompletion.choices[0].message.content);
+    return;
+  }
 
-  console.log(chatCompletion.choices[0].message.content);
+  for (let tool of toolcall) {
+    console.log(tool);
+  }
 }
 
 async function websearch({ query }) {
@@ -18,18 +25,42 @@ async function getGroqChatCompletion(args) {
   return groq.chat.completions.create({
     // temperature: form range 0 to 2
     // top_p (nuclear sampling): from range 0 to 1
+    model: "moonshotai/kimi-k2-instruct-0905",
     messages: [
       {
-        role: "assistant",
-        content:
-          "When giving output make sure its humble, clear and should end in max 5 lines in the form that first line contain only ---------------- the in next line output and then once output ends in next line -----------------",
+        role: "system",
+        content: `You are a smart assistant.
+          Respond to the user question and use tools if needed to answer the query.
+
+`,
       },
       {
         role: "user",
         content: `${args}`,
       },
     ],
-    model: "openai/gpt-oss-20b",
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "websearch",
+          description:
+            "Use this tool ONLY for real-time, latest, or current information.",
+          parameters: {
+            // JSON Schema object
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "The search query to perform search on",
+              },
+            },
+            required: [`query`],
+          },
+        },
+      },
+    ],
+    tool_choice: "auto",
   });
 }
 
